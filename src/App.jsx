98a839c9e0f7c34390eca7492b1d13839af3d388
Joongho <honor9320@gmail.com>
2026-02-23 01:35:51 +0900

    폰트, 이미지, 레이아웃 설정

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,32 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
 
+// 이모션 png 추가
+import { getEmotionImage } from "./util/get-emotion-images";
+
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 
 function App() {
+  // New 페이지로 이동 버튼
   const nav = useNavigate();
 
   const onClickButton = () => {
     nav("/new");
   };
+
   return (
     <>
       <div>
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
+      <div>
+        {/* 페이지 이동 버튼 */}
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
         <Link to={"/diary"}>Diary</Link>
@@ -26,7 +39,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
-        <Route path="/diary" element={<Diary />} />
+        <Route path="/diary/:id" element={<Diary />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
